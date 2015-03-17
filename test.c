23da63228566ba444bf4515216fc3873354b0cf1
S8aVv <leo.xuan@hotmail.com>
2015-03-17 23:53:08 +0800

    1st Version

---
+++ test.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: S8aVv.
+	> Mail:shaw.xuan820@gmail.com 
+	> Created Time: 二  3/17 21:58:31 2015
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc, char *argv[])
+{
+	printf("Hello World!\n");
+
+	return 0;
+}
